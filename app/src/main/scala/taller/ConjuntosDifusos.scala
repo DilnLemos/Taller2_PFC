bbaaf1078b1246b100fe0df4af47668cce0e72de
thetygerpotatto <letras.cj@gmail.com>
2024-10-04 15:37:03 -0500

    Correccion en las funciones union e interseccion

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -14,17 +14,17 @@ class ConjuntosDifusos {
 
     def muchoMayorQue(a: Int, m: Int): ConjDifuso = {
         def mma(x: Int): Double = {
-        if (x <= a) 0.0
-        else if (x > m) 1.0
-        else (x - a).toDouble / (m - a).toDouble
+            if (x <= a) 0.0
+            else if (x >= m) 1.0
+            else (x - a).toDouble / (m - a).toDouble
         }
-    mma
+        mma
     }
 
 
     def grande(d: Int, c: Int): ConjDifuso = {
-    require(c > 1, "c debe ser mayor que 1")
-    x => math.pow(x.toDouble / (x.toDouble + d), c)
+        require(c > 1, "c debe ser mayor que 1")
+        x => math.pow(x.toDouble / (x.toDouble + d), c)
     }
 
 
@@ -38,13 +38,13 @@ class ConjuntosDifusos {
 
     def union (cd1: ConjDifuso, cd2: ConjDifuso): ConjDifus = {
         // haremos la función de union de los conjuntos difusos
-        x => math.min(cd1(x), cd2(x))
+        x => math.max(cd1(x), cd2(x))
     }
 
 
     def interseccion (cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = {
         // haremos la función de intersección de los conjuntos difusos
-        x => math.max(cd1(x), cd2(x))
+        x => math.min(cd1(x), cd2(x))
 
 
     }
